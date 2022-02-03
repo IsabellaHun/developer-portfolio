adac29ddb8697c9e7cdbbb24e85b3cbce9526bb4
Isabella <itsbella2204@gmail.com>
2022-02-03 11:10:37 +0200

    added google analytics

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_ANALYTICS_ID: 'G-ZRY8T4ZBRW'
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
-import Layout from '../components/Layout.js'
-import '../styles/globals.css'
+import Layout from '../components/Layout.js';
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-        <Component {...pageProps} />
+      <Script
+      strategy='lazyOnload'
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}/>
+      <Script id='google-analytics-script' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-ZRY8T4ZBRW');
+        `}
+      </Script>
+      <Component {...pageProps} />
     </Layout>
   )}
 
